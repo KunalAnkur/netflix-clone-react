5afdab4a713aa9d11ecdc5546f70000b73a47480
Ankur Kunal <mrankur801@gmail.com>
2020-08-29 12:55:07 +0530

    first commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const APIKEY = "YOUR_TMDB_APIKEY";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetfilxOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+}
+
+export default requests
